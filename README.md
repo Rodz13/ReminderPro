04f56a946026ba5ab80ba76e1a12675ae811e828
Rodrigo <rodrigo.mirapalheta@pipedrive.com>
2019-12-30 02:50:16 +0200

    initial app setup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ReminderPro
 Start date 30.12.2019
+
+# Steps
+ - npm init -y
+ - dependencis: 
+  - express js: Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
+  - cors: https://flaviocopes.com/cors/
+  - nodemon - https://www.npmjs.com/package/nodemon
+  - concurrently - https://www.npmjs.com/package/concurrently
+
+- create index.js for the backend
+- create react-app client
+ - dependencis:
+    - axios: https://flaviocopes.com/axios/
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+    const user = req.query.user || "rodz13";
+    axios.get(`https://api.github.com/users/${user}`)
+    .then(response => {
+        res.json({ user: response.data});
+    });
+});
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    });
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
